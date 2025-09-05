49656d6e9eb572a7b75a087dec98b0e576039595
Njabulo <ST104697@rcconnect.edu.za>
2025-09-05 14:43:37 +0200

    Prompt user for number

--- src/main/java/com/mycompany/guessthenumber/GuessTheNumber.java
+++ src/main/java/com/mycompany/guessthenumber/GuessTheNumber.java
@@ -13,7 +13,16 @@ import java.util.Scanner;
 public class GuessTheNumber {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello World!");
+            int secretNumber = 7; // You can change this to any number you want the user to guess
+            int userGuess;
+
+        System.out.println("Guess a number between 1 and 10:");
+
+       
+
+            
+        }
     }
 }
